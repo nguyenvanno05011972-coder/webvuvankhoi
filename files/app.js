360a65e9fd999225b5311c5ea8ae52cd2282d02a
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2025-11-14 15:02:13 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -26,12 +26,12 @@ const apps = [
         descriptions: 'Hỗ Trợ iOS 14.0-16.6.1 Cập Nhật Ngày 14/11/2025'
     },    {
         name: '<strong><em>DeltaVN Font Mới ✅</em></strong>',
-        url: 'https://sub2s.com/1763048405',
+        url: 'https://sub2s.com/1763106937',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
         descriptions: 'Link Cài Trực Tiếp V698 Update Ngày 14/11/2025'
     },{    
         name: '<strong><em>KrnlVN Font Mới ✅</em></strong>',
-        url: 'https://sub2s.com/1763048355',
+        url: 'https://sub2s.com/1763106846',
         img: 'https://i.postimg.cc/B6ptwty4/5-DA6010-F-82-E9-46-E2-8870-794067574-ABE.jpg',
         descriptions: 'Link Cài Trực Tiếp V698 Update Ngày 14/11/2025'
     },{

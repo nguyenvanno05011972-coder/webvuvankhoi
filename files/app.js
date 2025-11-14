715baacc091d34f7004fa4f936a4660fba186b14
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2025-11-14 15:52:56 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -15,7 +15,7 @@ const apps = [
     },
     {
         name: '<strong><em> Hack Map Liên Quân NO KEY Full Chức Năng ✅</em></strong>',
-        url: 'https://sub2s.com/1763049253',
+        url: 'https://sub2s.com/1763110334',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
         descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 14/11/2025'
     },

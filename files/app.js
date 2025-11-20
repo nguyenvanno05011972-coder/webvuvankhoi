906662844f1afe5a3712d63db6219c9e80c6fe62
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2025-11-20 18:46:27 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -5,13 +5,13 @@ const apps = [
         name: '<strong><em> Quán Phở Anh Hai ✅</em></strong>',
         url: 'https://sub2s.com/1763366971',
         img: 'https://sf-static.upanhlaylink.com/img/image_202511096c9b69aee517d00a57c708e5023d9df3.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 17/11/2025'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 20/11/2025'
     },
     {
         name: '<strong><em> Hack Map Liên Quân NO KEY Full Chức Năng ✅</em></strong>',
         url: 'https://sub2s.com/1763366724',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 17/11/2025'
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 20/11/2025'
     },
     {
         name: '<strong><em>Troll Store X ✅</em></strong>',
@@ -50,13 +50,13 @@ const apps = [
         name: '<strong><em>FluxusZ VNG ANDROI V698 ✅</em></strong>',
         url: 'https://sub2s.com/1762918459',
         img: 'https://sf-static.upanhlaylink.com/img/image_202511046063dea8750bb9bfa463a5f879ee8aa4.jpg',
-        descriptions: 'Cập Nhật Ngày 14/11/2025'
+        descriptions: 'Cập Nhật Ngày 20/11/2025'
     },
       {
         name: '<strong><em>DELTA X VNG ANDROI V698 ✅</em></strong>',
         url: 'https://nhapcode1s.com/nbsQY6Z',
         img: 'https://sf-static.upanhlaylink.com/img/image_202511046063dea8750bb9bfa463a5f879ee8aa4.jpg',
-        descriptions: 'Cập Nhật Ngày 14/11/2025'
+        descriptions: 'Cập Nhật Ngày 20/11/2025'
     },{
         name: '<strong><em>KRNL VNG ANDROI V697 NO KEY ✅</em></strong>',
         url: 'https://nhapcode1s.com/nbXwbV8',

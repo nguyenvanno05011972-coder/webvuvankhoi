896901239b24f174bb38970a209d070d9a3373c6
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2025-12-07 09:53:24 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -21,7 +21,7 @@ const apps = [
         name: '<strong><em> Hack Map Liên Quân IOS NO KEY  ✅</em></strong>',
         url: 'https://sub2s.com/1763772164',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Key Free LQ `crackvtiosbyyoutubengocbonggaming` Sài Hết Mùa'
+        descriptions: 'Key Free LQ ( crackvtiosbyyoutubengocbonggaming ) Sài Hết Mùa'
     },{
         name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
         url: 'https://sub2s.com/1765071083',

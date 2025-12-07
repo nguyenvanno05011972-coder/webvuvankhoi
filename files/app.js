1a7e5b7b7b52e622696fb85023c07b8fbebcaef1
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2025-12-07 08:34:39 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -5,12 +5,12 @@ const apps = [
         name: '<strong><em> Hack Câu Cá Vạn Cân ANDROI ✅</em></strong>',
         url: 'https://nhapcode1s.com/nbzZdPn',
         img: 'https://sf-static.upanhlaylink.com/img/image_202511257920e27952a2addce80b406789bfb978.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 06/12/2025'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 07/12/2025'
     },{
         name: '<strong><em> Hack Speed Câu Cá Vạn Cân IOS ✅</em></strong>',
         url: 'https://sub2s.com/1764041630',
         img: 'https://sf-static.upanhlaylink.com/img/image_202511257920e27952a2addce80b406789bfb978.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 06/12/2025'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 07/12/2025'
     },{
         name: '<strong><em> Quán Phở Anh Hai ✅</em></strong>',
         url: 'https://sub2s.com/1763771985',
@@ -18,31 +18,36 @@ const apps = [
         descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 02/12/2025'
     },
     {
-        name: '<strong><em> Hack Map Liên Quân NO KEY Full Chức Năng ✅</em></strong>',
+        name: '<strong><em> Hack Map Liên Quân IOS NO KEY  ✅</em></strong>',
         url: 'https://sub2s.com/1763772164',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 06/12/2025'
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 07/12/2025'
+    },{
+        name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
+        url: 'https://sub2s.com/1765071083',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 07/12/2025'
     },
     {
         name: '<strong><em>Troll Store X ✅</em></strong>',
         url: 'https://sub2s.com/1764042583',
         img: 'https://raw.githubusercontent.com/alfiecg24/TrollInstallerX/main/Resources/Icon.png',
-        descriptions: 'Hỗ Trợ iOS 14.0-16.6.1 Cập Nhật Ngày 06/12/2025'
+        descriptions: 'Hỗ Trợ iOS 14.0-16.6.1 Cập Nhật Ngày 07/12/2025'
     },{ 
         name: '<strong><em>Skibx VNG IOS ✅</em></strong>',
         url: 'https://sub2s.com/1763772232',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251124601ed18d590b063f9cfac260dc055130.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.701 Update Ngày 06/12/2025'
+        descriptions: 'Link Cài Trực Tiếp V2.701 Update Ngày 07/12/2025'
     },{ 
         name: '<strong><em>DeltaVN Siêu Fix Lag ✅</em></strong>',
         url: 'https://sub2s.com/1763957772',
         img: 'https://sf-static.upanhlaylink.com/img/image_2025112480f23bac585e13c82db2053fa34ac240.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.701 Update Ngày 06/12/2025'
+        descriptions: 'Link Cài Trực Tiếp V2.701 Update Ngày 07/12/2025'
     },{ 
         name: '<strong><em>DeltaVN Font Thường ✅</em></strong>',
         url: 'https://sub2s.com/1763772293',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.701 Update Ngày 06/12/2025'
+        descriptions: 'Link Cài Trực Tiếp V2.701 Update Ngày 07/12/2025'
     },{ 
       
         name: '<strong><em>KrnlVN Font Thường ✅</em></strong>',

b45fc5202570d97a3d69ad6f37995c8667d70e4b
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2025-12-19 13:23:08 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -22,13 +22,13 @@ const apps = [
         name: '<strong><em> Hack Speed Câu Cá Vạn Cân IOS ✅</em></strong>',
         url: 'https://sub2s.com/1764041630',
         img: 'https://sf-static.upanhlaylink.com/img/image_202511257920e27952a2addce80b406789bfb978.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 13/12/2025'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 19/12/2025'
     },
     {
-        name: '<strong><em> Hack Map Liên Quân IOS NO KEY  ✅</em></strong>',
+        name: '<strong><em> Hack Map LQ Vthang IOS ✅</em></strong>',
         url: 'https://sub2s.com/1763772164',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Fix Lỗi Toàn Vẹn Update 13/12/2025'
+        descriptions: 'KEY FREE: Ngocbonggaming88ngocbonggaming Update 19/12/2025'
     },{
         name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
         url: 'https://sub2s.com/1765071083',

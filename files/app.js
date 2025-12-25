4f566aad26429a56cd8d1f174e15c1820114f28f
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2025-12-25 08:47:35 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -15,37 +15,37 @@ const apps = [
         img: 'https://sf-static.upanhlaylink.com/img/image_202512168b42655a46869ba4937b9819fb85f89e.jpg',
         descriptions: 'Menu Full Chức Năng Cập Nhật Ngày 21/12/2025'
     },                  {
-        name: '<strong><em> Free Fire IOS ✅</em></strong>',
+        name: '<strong><em> Menu Migul FF IOS ✅</em></strong>',
         url: 'https://sub2s.com/1766134765',
         img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
-        descriptions: 'Link Cài Trực Tiếp Có Mục Tệp Cập Nhật Ngày 19/12/2025'
+        descriptions: 'Link Cài Trực Tiếp Có Mục Tệp Cập Nhật Ngày 25/12/2025'
     }, 
                  {
         name: '<strong><em> Hack Menu Free Fire ANDROI ✅</em></strong>',
         url: 'https://sub2s.com/1765587408',
         img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
-        descriptions: 'Menu Full Chức Năng Cập Nhật Ngày 19/12/2025'
+        descriptions: 'Menu Full Chức Năng Cập Nhật Ngày 25/12/2025'
     }, {
         name: '<strong><em> Hack Câu Cá Vạn Cân ANDROI ✅</em></strong>',
         url: 'https://nhapcode1s.com/nbzZdPn',
         img: 'https://sf-static.upanhlaylink.com/img/image_202511257920e27952a2addce80b406789bfb978.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 19/12/2025'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 25/12/2025'
     },{
         name: '<strong><em> Hack Speed Câu Cá Vạn Cân IOS ✅</em></strong>',
         url: 'https://sub2s.com/1764041630',
         img: 'https://sf-static.upanhlaylink.com/img/image_202511257920e27952a2addce80b406789bfb978.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 19/12/2025'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 25/12/2025'
     },
     {
         name: '<strong><em> Hack Map LQ Vthang IOS ✅</em></strong>',
         url: 'https://sub2s.com/1763772164',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'KEY FREE: Ngocbonggaming88ngocbonggaming Update 19/12/2025'
+        descriptions: 'KEY FREE: Ngocbonggaming88ngocbonggaming Update 25/12/2025'
     },{
         name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
         url: 'https://sub2s.com/1765071083',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 19/12/2025'
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 25/12/2025'
     },
     {
         name: '<strong><em>Troll Store X ✅</em></strong>',
@@ -56,17 +56,17 @@ const apps = [
         name: '<strong><em>Skibx VNG IOS ✅</em></strong>',
         url: 'https://sub2s.com/1765774670',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251124601ed18d590b063f9cfac260dc055130.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.702 Update Ngày 19/12/2025'
+        descriptions: 'Link Cài Trực Tiếp V2.702 Update Ngày 25/12/2025'
     },{ 
         name: '<strong><em>DeltaVN Siêu Fix Lag ✅</em></strong>',
         url: 'https://sub2s.com/1766123854',
         img: 'https://sf-static.upanhlaylink.com/img/image_2025121933c057257eade3dbf9fbc48bb66a07cd.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.702 Update Ngày 19/12/2025'
+        descriptions: 'Link Cài Trực Tiếp V2.702 Update Ngày 25/12/2025'
     },{ 
         name: '<strong><em>DeltaVN Font Thường ✅</em></strong>',
         url: 'https://sub2s.com/1763772293',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.702 Update Ngày 19/12/2025'
+        descriptions: 'Link Cài Trực Tiếp V2.702 Update Ngày 25/12/2025'
     },{ 
       
         name: '<strong><em>KrnlVN Font Thường ✅</em></strong>',

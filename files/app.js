894a54a20211698ddafc93a8c55a2ee2ce713e4e
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-01-12 11:02:23 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -34,7 +34,7 @@ const apps = [
         name: '<strong><em> Hack Map LQ NO KEY IOS ✅</em></strong>',
         url: 'https://sub2s.com/1768190439',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Update 08/01/2026'
+        descriptions: 'Link Cài Trực Tiếp Update 12/01/2026'
     },{
         name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
         url: 'https://sub2s.com/1765071083',

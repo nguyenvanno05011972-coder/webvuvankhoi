3e21233b82903caabcfe7717800a94006cf06ec6
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-01-19 13:14:41 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -58,12 +58,12 @@ const apps = [
         name: '<strong><em>Skibx VNG IOS ✅</em></strong>',
         url: 'https://sub2s.com/1768802233',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251124601ed18d590b063f9cfac260dc055130.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.703 Update Ngày 19/01/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.704 Update Ngày 19/01/2026'
     },{ 
         name: '<strong><em>DeltaVN Font Thường ✅</em></strong>',
         url: 'https://sub2s.com/1768802912',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.703 Update Ngày 19/01/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.704 Update Ngày 19/01/2026'
     },{ 
       
         name: '<strong><em>KrnlVN Font Thường ✅</em></strong>',

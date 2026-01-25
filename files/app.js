b094a312d0464e1b9c1de8330a2fc6350fc9ab48
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-01-25 21:28:43 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -9,9 +9,9 @@ const apps = [
         descriptions: 'Qingdao Rural Commercial Bank Co., Ltd'   
     },{
         name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1769054826',
+        url: 'https://sub2s.com/1769351284',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Update 22/01/2026'
+        descriptions: 'Link Cài Trực Tiếp Update 25/01/2026'
     },{
         name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
         url: 'https://sub2s.com/1769012214',

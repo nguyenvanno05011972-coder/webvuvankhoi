0c1a38e7e402f7da527239bc3bf49f54037004d8
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-01-29 12:01:45 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -4,7 +4,7 @@ const apps = [
 
   { 
         name: '<strong><em>DeltaVN Font Thường ✅</em></strong>',
-        url: 'https://sub2s.com/1769618322',
+        url: 'https://sub2s.com/1769662871',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
         descriptions: 'Link Cài Trực Tiếp V2.704 Update Ngày 29/01/2026'
     },{

2c47d10b6ebdef22a1df5aeae9907ab3d9ece056
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-02-03 18:20:10 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -4,7 +4,7 @@ const apps = [
 
   { 
         name: '<strong><em>Delta VNG IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1770095825',
+        url: 'https://sub2s.com/1770117522',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
         descriptions: 'Link Cài Trực Tiếp V2.705 Update Ngày 03/02/2026'
     },{ 

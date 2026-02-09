bde357838a02b13f78ec45ebd5158ccf7013e1f2
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-02-09 10:04:21 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -2,7 +2,12 @@
 
 const apps = [
 
-    { 
+    {
+        name: '<strong><em> Hack FC Mobile IOS ✅</em></strong>',
+        url: 'https://sub2s.com/1770606242',
+        img: 'https://sf-static.upanhlaylink.com/img/image_20260119097c8816e45539efda49e93ea5ecb159.jpg',
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 09/02/2026'
+    },{ 
         name: '<strong><em>Hack 8 Ball Pool IOS ✅</em></strong>',
         url: 'https://sub2s.com/1770538320',
         img: 'https://sf-static.upanhlaylink.com/img/image_202602045a9f22c74187cd7acdd7fa73103c4f3f.jpg',
@@ -11,12 +16,12 @@ const apps = [
         name: '<strong><em>Delta VNG IOS ✅</em></strong>',
         url: 'https://sub2s.com/1770434238',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.706 Update Ngày 08/02/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.706 Update Ngày 09/02/2026'
     },{ 
         name: '<strong><em>Skibx VNG IOS ✅</em></strong>',
         url: 'https://sub2s.com/1770538215',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251124601ed18d590b063f9cfac260dc055130.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.706 Update Ngày 08/02/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.706 Update Ngày 09/02/2026'
     },{
         name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
         url: 'https://sub2s.com/1770503959',
@@ -43,11 +48,6 @@ const apps = [
         url: 'https://sub2s.com/1769012214',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
         descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 26/01/2026'
-    },{
-        name: '<strong><em> Hack FC Mobile IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1769733877',
-        img: 'https://sf-static.upanhlaylink.com/img/image_20260119097c8816e45539efda49e93ea5ecb159.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 30/01/2026'
     },{
         name: '<strong><em> Hack Cáu Cá Vạn Cân IOS ✅</em></strong>',
         url: 'https://sub2s.com/1768188919',

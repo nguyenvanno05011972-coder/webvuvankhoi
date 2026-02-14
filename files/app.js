0a98ae5bed356445ba96c60d35b5b84a20666937
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-02-14 09:00:01 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -38,6 +38,11 @@ const apps = [
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
         descriptions: 'Link Cài Trực Tiếp Update 14/02/2026'
     },{
+        name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
+        url: 'https://sub2s.com/1769012214',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 14/02/2026'
+    },{
         name: '<strong><em> Menu Hack FF IOS ✅</em></strong>',
         url: 'https://nhapcode1s.com/nb3AGrV',
         img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
@@ -48,11 +53,6 @@ const apps = [
         url: 'https://sub2s.com/1768736549',
         img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
         descriptions: 'Menu Full Chức Năng Cập Nhật Ngày 30/01/2026'
-    },{
-        name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
-        url: 'https://sub2s.com/1769012214',
-        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 26/01/2026'
     },{
         name: '<strong><em> Hack Cáu Cá Vạn Cân IOS ✅</em></strong>',
         url: 'https://sub2s.com/1768188919',

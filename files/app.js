ff355d0df18adaa0da63c968fdc13215a45f6052
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-02-21 09:19:14 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -4,14 +4,14 @@ const apps = [
 
     {
         name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1771571605',
+        url: 'https://sub2s.com/1771640243',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Update 20/02/2026'
+        descriptions: 'Link Cài Trực Tiếp Update 21/02/2026'
     },{
         name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
         url: 'https://sub2s.com/1769012214',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 20/02/2026'
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 21/02/2026'
     },{ 
         name: '<strong><em>Delta VNG IOS ✅</em></strong>',
         url: 'https://sub2s.com/1771380318',

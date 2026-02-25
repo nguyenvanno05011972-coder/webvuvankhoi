94f939dfd89cc8499ee62ad5218bb8174384e1ab
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-02-25 18:24:22 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -38,16 +38,6 @@ const apps = [
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
         descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 22/02/2026'
     },{
-        name: '<strong><em> Menu Hack FF IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1771143501',
-        img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 15/02/2026'
-    },{
-        name: '<strong><em> Menu Hack Free Fire ANDROI ✅</em></strong>',
-        url: 'https://sub2s.com/1768736549',
-        img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
-        descriptions: 'Menu Full Chức Năng Cập Nhật Ngày 15/01/2026'
-    },{
         name: '<strong><em> Hack FC Mobile IOS ✅</em></strong>',
         url: 'https://sub2s.com/1771144860',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260119097c8816e45539efda49e93ea5ecb159.jpg',

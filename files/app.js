d0de6144250ab8c38d47c55aa1e2b36ddd382298
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-03-16 19:02:37 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -1,7 +1,17 @@
 // APP DATA CONFIGURATION
 
 const apps = [
-{ 
+{
+        name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
+        url: 'https://tinyurl.com/38bep29m',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'Link Cài Trực Tiếp Update 16/03/2026'
+    },{
+        name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
+        url: 'https://sub2s.com/1771756845',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 16/03/2026'
+    }{ 
         name: '<strong><em>Delta VNG Fix Lag IOS ✅</em></strong>',
         url: 'https://sub2s.com/1773361227',
         img: 'https://sf-static.upanhlaylink.com/img/image_2026030220396b66d0eb107957a694b68f14f1c9.jpg',
@@ -26,16 +36,6 @@ const apps = [
         url: 'https://sub2s.com/1771927178',
         img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
         descriptions: 'Menu Full Chức Năng Cập Nhật Ngày 13/03/2026'
-    },{
-        name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1773361728',
-        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Update 13/03/2026'
-    },{
-        name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
-        url: 'https://sub2s.com/1771756845',
-        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 13/03/2026'
     },{
         name: '<strong><em> Hack FC Mobile IOS ✅</em></strong>',
         url: 'https://sub2s.com/1773361843',

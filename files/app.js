ddf15470ec0794ed0e3610ab072514b7125ff198
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-03-30 23:00:23 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -3,7 +3,7 @@
 const apps = [
 {
         name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
-        url: 'https://tinyurl.com/38bep29m',
+        url: 'https://nhapcode1s.com/api/AhQVxo',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
         descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 31/03/2026'
     },{

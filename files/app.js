d3a03593a3f970b1021a88b2ef23785654f5eead
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-04-16 14:03:09 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -13,19 +13,19 @@ const apps = [
         descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 15/04/2026'
     },{ 
         name: '<strong><em>Delta VNG IOS Siêu Fix Lag ✅</em></strong>',
-        url: 'https://ipas.cc/v2ykvq',
+        url: 'https://ipas.cc/nj0ji8',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260326e0fe878aec5a52921e12b228e0bdfecd.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.715 Update Ngày 15/04/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.715 Update Ngày 16/04/2026'
     },{ 
         name: '<strong><em>Delta VNG IOS ✅</em></strong>',
-        url: 'https://ipas.cc/k43lkf',
+        url: 'https://ipas.cc/g6sw6o',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.715 Update Ngày 15/04/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.715 Update Ngày 16/04/2026'
     },{ 
         name: '<strong><em>Skibx VNG IOS ✅</em></strong>',
         url: 'https://tinyurl.com/4raj86w8',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251124601ed18d590b063f9cfac260dc055130.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.715 UPDTAE Ngày 15/04/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.715 UPDTAE Ngày 16/04/2026'
     },{
         name: '<strong><em> Menu Hack FF IOS ✅</em></strong>',
         url: 'https://www.ngocbonggaming.com/2025/12/menu-hack-free-fire-androi-full-chuc.html',

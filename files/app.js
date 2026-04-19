f3e314b75052d3346d440c7b2ea8125214659570
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-04-19 07:31:47 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -5,37 +5,37 @@ const apps = [
         name: '<strong><em> Menu Hack FF IOS ✅</em></strong>',
         url: 'https://www.ngocbonggaming.com/2025/12/menu-hack-free-fire-androi-full-chuc.html',
         img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 18/04/2026'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 19/04/2026'
     },{
         name: '<strong><em> Menu Hack Free Fire ANDROI ✅</em></strong>',
         url: 'https://www.mediafire.com/file/0f71wq2i1qpa3ho/FFM+VIP+TRENDRAFT.apks/file',
         img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
-        descriptions: 'Menu Full Chức Năng Cập Nhật Ngày 18/04/2026'
+        descriptions: 'Menu Full Chức Năng Cập Nhật Ngày 19/04/2026'
     },{
         name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
         url: 'https://www.ngocbonggaming.com/2025/12/hack-map-unlock-skin-lien-quan-full.html',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 18/04/2026'
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 19/04/2026'
     },{
         name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
         url: 'https://www.ngocbonggaming.com/2026/04/hack-map-unlock-skin-lien-quan-androi.html',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 18/04/2026'
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 19/04/2026'
     },{ 
         name: '<strong><em>Delta VNG IOS Siêu Fix Lag ✅</em></strong>',
         url: 'https://ipas.cc/0mtbhs',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260326e0fe878aec5a52921e12b228e0bdfecd.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.715 Update Ngày 18/04/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.716 Update Ngày 19/04/2026'
     },{ 
         name: '<strong><em>Delta VNG IOS ✅</em></strong>',
         url: 'https://tinyurl.com/3jyc6a6w',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.715 Update Ngày 18/04/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.716 Update Ngày 19/04/2026'
     },{ 
         name: '<strong><em>Skibx VNG IOS ✅</em></strong>',
-        url: 'https://tinyurl.com/4raj86w8',
+        url: 'https://ipas.cc/lv795i',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251124601ed18d590b063f9cfac260dc055130.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.715 UPDTAE Ngày 18/04/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.716 UPDTAE Ngày 19/04/2026'
     },{
         name: '<strong><em> Hack FC Mobile IOS ✅</em></strong>',
         url: 'https://ipas.cc/5y939k',

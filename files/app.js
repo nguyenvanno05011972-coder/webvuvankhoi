7a513e69e490806810d3607782614c5de4c6c401
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-05-02 07:01:19 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -1,7 +1,27 @@
 // APP DATA CONFIGURATION
 
 const apps = [
-{
+{ 
+        name: '<strong><em>Delta VNG IOS Siêu Fix Lag ✅</em></strong>',
+        url: 'https://sub2s.com/1777680010',
+        img: 'https://sf-static.upanhlaylink.com/img/image_20260326e0fe878aec5a52921e12b228e0bdfecd.jpg',
+        descriptions: 'Link Cài Trực Tiếp V2.718 Update Ngày 02/05/2026'
+    },{ 
+        name: '<strong><em>Delta VNG IOS ✅</em></strong>',
+        url: 'https://sub2s.com/1777641953',
+        img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
+        descriptions: 'Link Cài Trực Tiếp V2.718 Update Ngày 02/05/2026'
+    },{
+        name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
+        url: 'https://sub2s.com/1777641895',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 02/05/2026'
+    },{
+        name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
+        url: 'https://sub2s.com/1777641916',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 02/05/2026'
+    },{
         name: '<strong><em> Minecraft PE 26.13 IOS ✅</em></strong>',
         url: 'https://sub2s.com/1777641767',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260425de927ff2fe8af488befd188e2b45d2c8.jpg',
@@ -11,26 +31,6 @@ const apps = [
         url: 'https://sub2s.com/1777641785',
         img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
         descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 01/05/2026'
-    },{
-        name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1777641895',
-        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 01/05/2026'
-    },{
-        name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
-        url: 'https://sub2s.com/1777641916',
-        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 01/05/2026'
-    },{ 
-        name: '<strong><em>Delta VNG IOS Siêu Fix Lag ✅</em></strong>',
-        url: 'https://sub2s.com/1777641935',
-        img: 'https://sf-static.upanhlaylink.com/img/image_20260326e0fe878aec5a52921e12b228e0bdfecd.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.718 Update Ngày 01/05/2026'
-    },{ 
-        name: '<strong><em>Delta VNG IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1777641953',
-        img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.718 Update Ngày 01/05/2026'
     },{ 
         name: '<strong><em>HACK CFL MOBILE IOS ✅</em></strong>',
         url: 'https://sub2s.com/1777641975',

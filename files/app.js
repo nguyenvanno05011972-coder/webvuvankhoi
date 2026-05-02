0c15e13fca3b7c72ea27bd1940fdd83a27088eee
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-05-02 15:20:49 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -28,7 +28,7 @@ const apps = [
         descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 02/05/2026'
     },{ 
         name: '<strong><em>Delta VNG IOS Siêu Fix Lag ✅</em></strong>',
-        url: 'https://sub2s.com/1777680010',
+        url: 'https://sub2s.com/1777710031',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260326e0fe878aec5a52921e12b228e0bdfecd.jpg',
         descriptions: 'Link Cài Trực Tiếp V2.718 Update Ngày 02/05/2026'
     },{ 

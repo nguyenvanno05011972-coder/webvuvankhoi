a2b80f74f9d4695987e37f7a8aa9d6b63ef804ba
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-05-05 11:56:19 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -1,26 +1,31 @@
 // APP DATA CONFIGURATION
 
 const apps = [
-{ 
+{
+        name: '<strong><em> Menu Hack FF IOS ✅</em></strong>',
+        url: 'https://sub2s.com/1776913657',
+        img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 05/05/2026'
+    },{ 
         name: '<strong><em>Delta VNG IOS Siêu Fix Lag ✅</em></strong>',
         url: 'https://sub2s.com/1777787162',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260326e0fe878aec5a52921e12b228e0bdfecd.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.718 Update Ngày 04/05/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.718 Update Ngày 05/05/2026'
     },{ 
         name: '<strong><em>Delta VNG IOS ✅</em></strong>',
         url: 'https://sub2s.com/1777786929',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.718 Update Ngày 04/05/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.718 Update Ngày 05/05/2026'
     },{
         name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
         url: 'https://sub2s.com/1777680154',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 04/05/2026'
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 05/05/2026'
     },{
         name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
         url: 'https://sub2s.com/1777641916',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 04/05/2026'
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 05/05/2026'
     },{
         name: '<strong><em> Minecraft PE 26.13 IOS ✅</em></strong>',
         url: 'https://sub2s.com/1777702457',
@@ -31,11 +36,6 @@ const apps = [
         url: 'https://sub2s.com/1777709739',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260429379c41d9aaf4a816c969e28bac3e2660.jpg',
         descriptions: 'Link Cài Trực Tiếp -Update Ngày 02/05/2026'
-    },{
-        name: '<strong><em> Menu Hack FF IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1777701207',
-        img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 02/05/2026'
     },{ 
         name: '<strong><em>Hack 8 Ball Pool IOS ✅</em></strong>',
         url: 'https://sub2s.com/1777702932',

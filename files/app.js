d117dd6b1ea666ce58c589699c8ff9c961d64eea
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-05-08 11:45:44 +0700

    Add files via upload

---
+++ files/app.js
@@ -0,0 +1,442 @@
+// APP DATA CONFIGURATION
+
+const apps = [
+    {
+        name: '𝐋𝐢𝐞̂𝐧 𝐐𝐮𝐚̂𝐧 𝐌𝐨𝐛𝐢𝐥𝐞 Hack Map ✅ ',
+        url: 'https://bbmkts.com/go/aovhack',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'New Update'
+    },
+    {
+        name: '<strong><em>KEY HACK Liên Quân Mobile 30 NGÀY ✅</em></strong>',
+        url: 'https://bbmkts.com/go/keyaov',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'New Update'
+    },
+    {
+        name: 'TrollStore X ✅',
+        url: 'https://bbmkts.com/go/troll',
+        img: 'https://raw.githubusercontent.com/alfiecg24/TrollInstallerX/main/Resources/Icon.png',
+        descriptions: 'New Update'
+    },
+    {
+        name: 'Krnl VN ✅',
+        url: 'https://bbmkts.com/go/ipakrnl',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/0f/cd/0c/0fcd0c28-8cea-2165-85f0-731e14618522/AppIcon_VNGGames_New-0-0-1x_U007epad-0-1-0-85-220.png/512x512bb.jpg',
+        descriptions: 'New Update'
+    },
+    {
+        name: 'DeltaVN ✅',
+        url: 'https://bbmkts.com/go/ipadelta',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/0f/cd/0c/0fcd0c28-8cea-2165-85f0-731e14618522/AppIcon_VNGGames_New-0-0-1x_U007epad-0-1-0-85-220.png/512x512bb.jpg',
+        descriptions: 'New Update'
+    },
+    {
+        name: 'Krnl Global ✅',
+        url: 'https://bbmkts.com/go/ipakrnlqt',
+        img: 'https://sf-static.upanhlaylink.com/img/image_2025092931c4f85956a4f1ed99daa1271103e687.jpg',
+        descriptions: 'New Update'
+    },
+    {
+        name: 'Delta Global ✅',
+        url: 'https://bbmkts.com/go/ipadeltaqt',
+        img: 'https://sf-static.upanhlaylink.com/img/image_2025092931c4f85956a4f1ed99daa1271103e687.jpg',
+        descriptions: 'New Update'
+    },
+    // {
+    //     name: 'File iPA Liên Quân',
+    //     url: 'https://bbmkts.com/go/allaov',
+    //     img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+    //     descriptions: 'Gồm file 2 iPA Map Và Mod Skin Cập Nhật Ngày 20/10/2025'
+    // },{
+    //     name: 'File iPA Roblox VN',
+    //     url: 'https://bbmkts.com/go/allroblox',
+    //     img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/0f/cd/0c/0fcd0c28-8cea-2165-85f0-731e14618522/AppIcon_VNGGames_New-0-0-1x_U007epad-0-1-0-85-220.png/512x512bb.jpg',
+    //     descriptions: 'Gồm file 2 iPA Krnl VN Và Delta VN Cập Nhật Ngày 20/10/2025'
+    // },
+];
+
+const ksignApps = [
+        {
+        name: 'KSign New Cert ✅',
+        url: 'http://bit.ly/4nu8368',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'Guangzhou Huahan Educational & Technology Co.,Ltd.'   
+    },
+    {
+        name: 'KSign V3 China ✅',
+        url: 'https://loadly.io/6dFQMxip',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'China Telecommunications Corporation'   
+    },{
+        name: 'KSign V2 China ✅',
+        url: 'https://loadly.io/bfRwXm8B',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'China Telecommunications Corporation'   
+    },{
+        name: 'KSign V1 China ✅',
+        url: 'https://loadly.io/rFb2pKw8',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'China Telecommunications Corporation'   
+    },{
+        name: 'KSign ✅',
+        url: 'https://loadly.io/AOk7uhFE',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'China Telecommunications Corporation'   
+    },{
+        name: 'KSign GOBAL V7',
+        url: 'https://loadly.io/RhGmivnV',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'   
+    },{
+        name: 'KSign GOBAL V6',
+        url: 'https://loadly.io/2GoieWqg',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'   
+    },{
+        name: 'KSign GOBAL V5',
+        url: 'https://loadly.io/Vj8nRH5T',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'   
+    },{
+        name: 'KSign GOBAL V4',
+        url: 'https://loadly.io/kKLxKTOQ',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'   
+    },{
+        name: 'KSign GOBAL V3',
+        url: 'https://loadly.io/OcBGRnJK',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'   
+    },{
+        name: 'KSign GOBAL V2',
+        url: 'https://loadly.io/q7NuJqzk',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'   
+    },{
+        name: 'KSign GOBAL V1',
+        url: 'https://i.loadly.io/tjQMARBC',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'   
+    },{
+        name: 'KSign',
+        url: 'https://loadly.io/B4yjz3p6',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'   
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/07DJmaef',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'Tianjin University of Commerce'   
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/57jdwiXt',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'BMW Brilliance Automotive Ltd'   
+    },
+    {
+        name: 'KSign BMW V1',
+        url: 'https://loadly.io/dKaQsrKZ',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'BMW Brilliance Automotive Ltd'   
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/vQvSH91u',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE'   
+    },
+    {
+        name: 'KSign VNJ V1',
+        url: 'https://loadly.io/YW8HSTgM',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE'   
+    },
+    {
+        name: 'KSign VNJ V2',
+        url: 'https://loadly.io/LiQXptmW',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE'   
+    },
+    {
+        name: 'KSign VNJ V3',
+        url: 'https://loadly.io/TADbykHo',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE'   
+    },
+    {
+        name: 'KSign VNJ V4',
+        url: 'https://loadly.io/UZO7qsci',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE'   
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/pJE8Gd6X',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'VIETTEL GROUP'   
+    },
+    {
+        name: 'KSign VIETTEL GROUP V1',
+        url: 'https://loadly.io/YqfVTduO',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'VIETTEL GROUP'   
+    },
+    {
+        name: 'KSign VIETTEL GROUP V2',
+        url: 'https://loadly.io/XYvyFUF5',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'VIETTEL GROUP'   
+    },
+    {
+        name: 'KSign VIETTEL GROUP V3',
+        url: 'https://loadly.io/yOvQTfs8',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'VIETTEL GROUP'   
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/6kFySVCG',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY'   
+    },
+    {
+        name: 'KSign Chiba V1',
+        url: 'https://loadly.io/zUyCT3H8',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY'   
+    },
+    {
+        name: 'KSign Chiaba V2',
+        url: 'https://loadly.io/kSkjPMhv',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY'   
+    },
+    {
+        name: 'KSign Chiaba V3',
+        url: 'https://loadly.io/NRkK7p17',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY'   
+    },
+    {
+        name: 'KSign Chiba V4',
+        url: 'https://loadly.io/gb2XXQOu',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY'   
+    },
+    {
+        name: 'KSign Chiba V5',
+        url: 'https://loadly.io/Mp7femdt',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY'   
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/ubn8wwZY',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'TCL household Appliance Marketing Co'   
+    },
+    {
+        name: 'KSign TCL V1',
+        url: 'https://loadly.io/nWsaaiiL',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'TCL household Appliance Marketing Co'   
+    },
+    {
+        name: 'KSign TCL V2',
+        url: 'https://loadly.io/6i1ZZSji',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'TCL household Appliance Marketing Co'   
+    },
+    {
+        name: 'KSign TCL V3',
+        url: 'https://loadly.io/nhMdwJJX',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'TCL household Appliance Marketing Co'
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/dbEiV4Fr',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'GAC TOYOTA MOTOR CO'
+    },
+    {
+        name: 'KSign GAC V1',
+        url: 'https://loadly.io/9TnVohcF',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'GAC TOYOTA MOTOR CO'
+    },
+    {
+        name: 'KSign GAC V2',
+        url: 'https://loadly.io/ZHFbgrf3',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'GAC TOYOTA MOTOR CO'
+    },
+    {
+        name: 'KSign GAC V3',
+        url: 'https://loadly.io/qP7wRRqS',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'GAC TOYOTA MOTOR CO'
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/WoTRbYH7',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'EEO Education Ltd'
+    },
+    {
+        name: 'KSign EEO V1',
+        url: 'https://loadly.io/vu7OjCg1',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'EEO Education Ltd'
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/SpCyGaYM',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'China Railway Eryuan Engineering Group'
+    },
+    {
+        name: 'KSign China Railway V1',
+        url: 'https://loadly.io/EArBL5WZ',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'China Railway Eryuan Engineering Group'
+    },
+    {
+        name: 'KSign',
+        url: 'https://loadly.io/3psivxUU',
+        img: 'https://khoindvn.com/files/ksign.png',
+        descriptions: 'Beijing Esensoft Co'
+    }
+];
+
+const esignApps = [
+   {
+        name: 'ESign',
+        url: 'https://loadly.io/PGwC04B6',
+        img: 'https://khoindvn.com/files/profile-pic.png',
+        descriptions: 'GLOBAL TAKEOFF, INC'   
+    },
+    {
+        name: 'ESign ',
+        url: 'https://loadly.io/g0FwZsy3',
+        img: 'https://khoindvn.com/files/profile-pic.png',
+        descriptions: 'Tianjin University of Commerce'   
+    },
+    {
+        name: 'ESign ',
+        url: 'https://loadly.io/KDxT6fCO',
+        img: 'https://khoindvn.com/files/profile-pic.png',
+        descriptions: 'BMW Brilliance Automotive Ltd'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/vnjes8ios',
+        img: 'https://khoindvn.com/files/profile-pic.png',
+        descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/sCxKfNji',
+        img: 'https://khoindvn.com/files/profile-pic.png',
+        descriptions: 'VIETTEL GROUP'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/NRqDTAah',
+        img: 'https://khoindvn.com/files/profile-pic.png',
+        descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/8FnyqEjs',
+        img: 'https://khoindvn.com/files/profile-pic.png',
+        descriptions: 'Beijing Esensoft Co., Ltd'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/ZQgRwTtA',
+        img: 'https://khoindvn.com/files/profile-pic.png',
+        descriptions: 'NREH ESTATE INFORMATION TECHNOLOGY CO.,LTD'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/TM5Ye1O1',
+        img: 'https://khoindvn.com/files/profile-pic.png',
+        descriptions: 'China Railway Eryuan Engineering Group Co., Ltd.'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/Rod6cWdW',
+        img: 'https://khoindvn.com/files/profile-pic.png',
+        descriptions: 'EEO Education ltd'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/pokXH5G4',
+        img: 'https://khoindvn.com/files/profile-pic.png',
+        descriptions: 'TCL household Appliance Marketing Co., LTD'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/zBXxhRoc',
+        img: 'https://khoindvn.com/files/profile-pic.png',
+        descriptions: 'Wuling Power Corporation'   
+    },
+    {
+        name: 'ESign',
+        url: 'https://loadly.io/55ZWv8OZ',
+        img: 'https://khoindvn.com/files/profile-pic.png',
+        descriptions: 'GAC TOYOTA MOTOR CO.,LTD'
+    }
+    
+];
+
+// UI COMPONENTS
+
+const cloudIcon = `<svg class="icon" style="width: 24px;height: 24px;vertical-align: middle;fill: currentColor;overflow: hidden;" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="739"><path d="M691.3 693.9c-24.7 0-44.8-20-44.8-44.8 0-24.7 20-44.8 44.8-44.8h40c190.6 0 176.8-274.4 0.2-268.1C671.2 37.2 208 145.1 299 445c-93-73.9-204.6 69.2-109.6 139.8 67.2 49.9 188.5-18.4 188.5 64.3 0 56.3-63.9 44.8-125.3 44.8-149.3 0-241.3-159.3-155.8-281.1 40.8-58.2 104.9-72.6 104.9-72.6C231.4 111.3 492.7-9.5 686.2 120c83.1 55.6 114.5 136.9 114.5 136.9 48.8 14.8 90.7 45.6 119.2 86.4 96 137.3 21.5 350.6-228.6 350.6zM514.2 335.8c0.2 0 0.3 0.1 0.5 0.1s0.3-0.1 0.5-0.1h-1z m177.1 421.1c0-17.5-45.8-17.3-63.3 0.1l-32.8 32.9c-14.1 14.1-38.2 4.2-38.2-15.8V380.6c0-24.5-17.9-44.4-42.3-44.7-24.5 0.3-47.2 20.1-47.2 44.7v393.5c0 19.9-24.1 29.9-38.2 15.8l-32.8-42.1c-17.5-17.5-45.3-9.3-62.8-9.3h-0.2c-17.5 44.8-17.6 55.1-0.1 72.6L449 931.5c35 35 91.6 37.3 126.6 2.3l115.7-114.6c17.5-17.5 0-45.2 0-62.7v0.4z" p-id="740"></path></svg>`;
+
+// DOM ELEMENTS
+
+const appContainer = document.querySelector('.app-sections');
+
+// UTILITY FUNCTIONS
+
+function render(name, url, img, description) {
+    return `<div class="app-container">
+        <div class="app">
+            <img src="${img}" class="app-img box-shadow" alt="${name}">
+            <h1 style="margin-left: 1em">${name}<br>
+                <p>${description}</p>
+            </h1>
+        </div>
+        <i href="${url}" class="download-btn" style="color: #007aff;">
+            ${cloudIcon}
+        </i>
+    </div>`;
+}
+
+function addClickHandler(container) {
+    if (!container) return;
+    
+    container.addEventListener('click', (event) => {
+        const target = event.target.closest('.app-container');
+        if (target) {
+            const url = target.querySelector('.download-btn').getAttribute('href');
+            window.open(url, '_blank');
+        }
+    });
+}
+
+// INITIALIZATION
+
+// Render apps in their respective containers
+if (appContainer) {
+    apps.forEach(apps => {
+        appContainer.innerHTML += render(apps.name, apps.url, apps.img, apps.descriptions);
+    });
+}
+// Add click handlers to all containers
+addClickHandler(appContainer);

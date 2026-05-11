5ccbdf5bf3d0880f0982dab598cf7e30f0a1802d
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-05-11 16:39:17 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -3,49 +3,49 @@
 const apps = [
 {
         name: '<strong><em> Minecraft PE 26.13 IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1778219632',
+        url: 'https://sub2s.com/1778492137',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260425de927ff2fe8af488befd188e2b45d2c8.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 08/05/2026'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 11/05/2026'
     },{ 
         name: '<strong><em>Delta VNG IOS Siêu Fix Lag ✅</em></strong>',
-        url: 'https://sub2s.com/1778219530',
+        url: 'https://sub2s.com/1778492204',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260326e0fe878aec5a52921e12b228e0bdfecd.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.718 Update Ngày 08/05/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.718 Update Ngày 11/05/2026'
     },{ 
         name: '<strong><em>Delta VNG IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1778219564',
+        url: 'https://sub2s.com/1778492174',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.718 Update Ngày 08/05/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.718 Update Ngày 11/05/2026'
     },{
         name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1777680154',
+        url: 'https://sub2s.com/1778492243',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 08/05/2026'
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 11/05/2026'
     },{
         name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
         url: 'https://sub2s.com/1777641916',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 08/05/2026'
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 11/05/2026'
     },{ 
         name: '<strong><em>HACK PUBG MOBILE IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1778219672',
+        url: 'https://sub2s.com/1778492277',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260429379c41d9aaf4a816c969e28bac3e2660.jpg',
-        descriptions: 'Link Cài Trực Tiếp -Update Ngày 08/05/2026'
+        descriptions: 'Link Cài Trực Tiếp -Update Ngày 11/05/2026'
     },{
         name: '<strong><em> Menu Hack FF IOS ✅</em></strong>',
         url: 'https://sub2s.com/1777701207',
         img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 08/05/2026'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 11/05/2026'
     },{ 
         name: '<strong><em>Hack 8 Ball Pool IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1778219770',
+        url: 'https://sub2s.com/1778492315',
         img: 'https://sf-static.upanhlaylink.com/img/image_202602045a9f22c74187cd7acdd7fa73103c4f3f.jpg',
-        descriptions: 'Link Cài Trực Tiếp -Update Ngày 08/05/2026'
+        descriptions: 'Link Cài Trực Tiếp -Update Ngày 11/05/2026'
     },{
         name: '<strong><em> Hack FC Mobile IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1778219736',
+        url: 'https://sub2s.com/1778492344',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260119097c8816e45539efda49e93ea5ecb159.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 08/05/2026'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 11/05/2026'
     },{ 
         name: '<strong><em>HACK CFL MOBILE IOS ✅</em></strong>',
         url: 'https://sub2s.com/1777641975',

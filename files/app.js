fb9005d12136ec93dfaf0554f9e3c0f072dd9354
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-05-23 16:22:47 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -2,35 +2,35 @@
 
 const apps = [
 {
+        name: '<strong><em> Minecraft PE 26.13 IOS ✅</em></strong>',
+        url: 'https://funlink.io/PvK8Ao2',
+        img: 'https://sf-static.upanhlaylink.com/img/image_20260425de927ff2fe8af488befd188e2b45d2c8.jpg',
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 23/05/2026'
+    },{
         name: '<strong><em> Menu Hack FF IOS ✅</em></strong>',
         url: 'https://nhapcode1s.com/nb1VsH0',
         img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 21/05/2026'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 23/05/2026'
     },{ 
         name: '<strong><em>Delta VNG IOS Siêu Fix Lag ✅</em></strong>',
         url: 'https://sub2s.com/1778541846',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260326e0fe878aec5a52921e12b228e0bdfecd.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.721 Update Ngày 21/05/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.721 Update Ngày 23/05/2026'
     },{ 
         name: '<strong><em>Delta VNG IOS Bản Gốc ✅</em></strong>',
         url: 'https://sub2s.com/1778541846',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.721 Update Ngày 21/05/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.721 Update Ngày 23/05/2026'
     },{ 
         name: '<strong><em>HACK PUBG MOBILE IOS ✅</em></strong>',
         url: 'https://sub2s.com/1778909474',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260429379c41d9aaf4a816c969e28bac3e2660.jpg',
-        descriptions: 'Link Cài Trực Tiếp - Update Ngày 16/05/2026'
+        descriptions: 'Link Cài Trực Tiếp - Update Ngày 23/05/2026'
     },{ 
         name: '<strong><em>Geometry Dash PE IOS ✅</em></strong>',
         url: 'https://sub2s.com/1778596469',
         img: 'https://sf-static.upanhlaylink.com/img/image_2026051267e7fcfdc0840fd3a1ded82a70f1d231.jpg',
         descriptions: 'Link Cài Trực Tiếp Update Ngày 12/05/2026'
-    },{
-        name: '<strong><em> Minecraft PE 26.13 IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1778492137',
-        img: 'https://sf-static.upanhlaylink.com/img/image_20260425de927ff2fe8af488befd188e2b45d2c8.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 11/05/2026'
     },{
         name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
         url: 'https://sub2s.com/1778492243',

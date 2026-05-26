3649b2d32e259bd6e98d9ccd615024b062d4b311
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-05-26 10:27:41 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -2,6 +2,16 @@
 
 const apps = [
 {
+        name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
+        url: 'https://ipas.cc/bzg4ug',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 26/05/2026'
+    },{
+        name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
+        url: 'https://sub2s.com/1777641916',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 2605/2026'
+    },{
         name: '<strong><em> Minecraft PE 26.13 IOS ✅</em></strong>',
         url: 'https://funlink.io/PvK8Ao2',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260425de927ff2fe8af488befd188e2b45d2c8.jpg',
@@ -31,16 +41,6 @@ const apps = [
         url: 'https://sub2s.com/1778596469',
         img: 'https://sf-static.upanhlaylink.com/img/image_2026051267e7fcfdc0840fd3a1ded82a70f1d231.jpg',
         descriptions: 'Link Cài Trực Tiếp Update Ngày 12/05/2026'
-    },{
-        name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
-        url: 'https://sub2s.com/1778492243',
-        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 11/05/2026'
-    },{
-        name: '<strong><em> Hack Map + Unlock Skin Androi ✅</em></strong>',
-        url: 'https://sub2s.com/1777641916',
-        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 11/05/2026'
     },{ 
         name: '<strong><em>Hack 8 Ball Pool IOS ✅</em></strong>',
         url: 'https://sub2s.com/1778492315',

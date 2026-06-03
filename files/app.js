2555dd61e292b0e9fc9ea83d062566f9aa818c35
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-06-03 07:30:03 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -1,7 +1,17 @@
 // APP DATA CONFIGURATION
 
 const apps = [
-{
+{ 
+        name: '<strong><em>Delta VNG IOS Bản Gốc ✅</em></strong>',
+        url: 'https://tinyurl.com/yba9az2w',
+        img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
+        descriptions: 'Link Cài Trực Tiếp V2.723 Update Ngày 03/06/2026'
+    },{ 
+        name: '<strong><em>Delta VNG IOS Siêu Fix Lag ✅</em></strong>',
+        url: 'https://tinyurl.com/mr3ty348',
+        img: 'https://sf-static.upanhlaylink.com/img/image_20260326e0fe878aec5a52921e12b228e0bdfecd.jpg',
+        descriptions: 'Link Cài Trực Tiếp V2.723 Update Ngày 03/06/2026'
+    },{
         name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
         url: 'https://ipas.cc/7e6byd',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',

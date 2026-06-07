b9f65e4c41ea7f6a130ded4b1ccdc01c3a1d5591
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-06-07 19:21:25 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -3,9 +3,9 @@
 const apps = [
 {
         name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
-        url: 'https://ipas.cc/hnqy27',
+        url: 'https://ipas.cc/wgt4qm',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 06/06/2026'
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 07/06/2026'
     },{
         name: '<strong><em> Menu Hack FF IOS ✅</em></strong>',
         url: 'https://sub2s.com/1780703644',

aadc07a6fc74675bb9dbf69f1a0fc9bd4ce2533d
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-06-15 20:45:04 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -1,31 +1,31 @@
 // APP DATA CONFIGURATION
 
 const apps = [
-{ 
+{
+        name: '<strong><em> Minecraft PE V1.26.21 IOS ✅</em></strong>',
+        url: 'https://ipas.cc/21nz0r',
+        img: 'https://sf-static.upanhlaylink.com/img/image_20260425de927ff2fe8af488befd188e2b45d2c8.jpg',
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 15/06/2026'
+    },{ 
         name: '<strong><em>Delta VNG IOS Bản Gốc ✅</em></strong>',
         url: 'https://tinyurl.com/yba9az2w',
         img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.724 Update Ngày 14/06/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.724 Update Ngày 15/06/2026'
     },{ 
         name: '<strong><em>Delta VNG IOS Siêu Fix Lag ✅</em></strong>',
         url: 'https://ipas.cc/l8ng37',
         img: 'https://sf-static.upanhlaylink.com/img/image_20260326e0fe878aec5a52921e12b228e0bdfecd.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.724 Update Ngày 14/06/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.724 Update Ngày 15/06/2026'
     },{
         name: '<strong><em> Hack Map + Unlock Skin IOS ✅</em></strong>',
         url: 'https://tinyurl.com/57n8r5b5',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 13/06/2026'
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 15/06/2026'
     },{
         name: '<strong><em> Menu Hack FF IOS ✅</em></strong>',
         url: 'https://tinyurl.com/m2x57py8',
         img: 'https://sf-static.upanhlaylink.com/img/image_202512139629dca6ccd0fa07584c754bf9c9ec5b.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 13/06/2026'
-    },{
-        name: '<strong><em> Minecraft PE 26.13 IOS ✅</em></strong>',
-        url: 'https://tinyurl.com/bddduknu',
-        img: 'https://sf-static.upanhlaylink.com/img/image_20260425de927ff2fe8af488befd188e2b45d2c8.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 13/06/2026'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 15/06/2026'
     },{ 
         name: '<strong><em>HACK PUBG MOBILE IOS ✅</em></strong>',
         url: 'https://ipas.cc/5ie46k',

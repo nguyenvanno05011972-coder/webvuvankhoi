f05fb5e1af563c796606a4c425db952471a0464f
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-07-04 15:22:52 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -7,11 +7,6 @@ const apps = [
         img: 'https://sf-static.upanhlaylink.com/img/image_20251113ad21f5694d529e79bf61bff306fdd2f1.jpg',
         descriptions: 'Link Cài Trực Tiếp V2.727 Update Ngày 03/07/2026'
     },{ 
-        name: '<strong><em>Delta X VNG IOS Fix Lag ✅</em></strong>',
-        url: 'https://ipas.cc/zeggy6',
-        img: 'https://i.postimg.cc/yNfhpzs0/IMG-3417.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.727 Update Ngày 03/07/2026'
-    },{ 
         name: '<strong><em>Delta X IOS Quốc Tế ✅</em></strong>',
         url: 'https://tinyurl.com/mr3ty348',
         img: 'https://i.postimg.cc/W38YSZCD/1783077575726-541494777965378080-2631907376116522021-ee9b66260a9431c82f5bce787354198a.jpg',

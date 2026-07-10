d32c5db2a5523ebe4480620b99b47644ddac95cf
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-07-11 06:29:18 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -4,9 +4,9 @@ const apps = [
 { 
       
         name: '<strong><em>Delta X VNG IOS ✅</em></strong>',
-        url: 'https://ipas.cc/g09ur8',
+        url: 'https://ipas.cc/4ydxvo',
         img: 'https://i.postimg.cc/zBDfHPcR/IMG-3155.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.727 Cập Nhật Ngày 10/07/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.727 Cập Nhật Ngày 11/07/2026'
     },{ 
         name: '<strong><em>DELTA X IOS QUỐC TẾ✅</em></strong>',
         url: 'https://ipas.cc/bdwd7h',

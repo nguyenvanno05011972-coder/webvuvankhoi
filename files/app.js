3c5b77f1dd8b631f2d63a36e1bde686bca664655
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-07-17 15:23:10 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -3,22 +3,22 @@
 const apps = [
 { 
         name: '<strong><em>Delta X VNG IOS Bản Gốc ✅</em></strong>',
-        url: 'https://ipas.cc/7e0yp5',
+        url: 'https://ipas.cc/nhfqg7',
         img: 'https://i.postimg.cc/j5Pt7VJt/IMG-3155.jpg',
         descriptions: 'Link Cài Trực Tiếp V2.727 Cập Nhật Ngày 17/07/2026'
     },{ 
         name: '<strong><em>Delta X VNG IOS Fix Lag ✅</em></strong>',
-        url: 'https://ipas.cc/vu8tyt',
+        url: 'https://ipas.cc/zgj15z',
         img: 'https://i.postimg.cc/pXQNsVQs/IMG-3417.jpg',
         descriptions: 'Link Cài Trực Tiếp V2.727 Cập Nhật Ngày 17/07/2026'
     },{ 
         name: '<strong><em>Delta X Quốc Tế IOS  ✅</em></strong>',
-        url: 'https://ipas.cc/jcvrgh',
+        url: 'https://ipas.cc/kbaova',
         img: 'https://i.postimg.cc/wMkzwh26/IMG-4719.jpg',
         descriptions: 'Link Cài Trực Tiếp V2.727 Cập Nhật Ngày 17/07/2026'
     },{
         name: '<strong><em> MENU Liên Quân IOS ✅</em></strong>',
-        url: 'https://ipas.cc/27ow1m',
+        url: 'https://ipas.cc/j4dwum',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
         descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 17/07/2026'
     },{

26ee8966a697195c8413013eab2a8cc4e69963ce
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-07-17 23:16:51 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -2,6 +2,11 @@
 
 const apps = [
 {
+        name: '<strong><em> MENU Liên Quân IOS ✅</em></strong>',
+        url: 'https://testflight.apple.com/join/AyEMdCUz',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 18/07/2026'
+    },{
         name: '<strong><em> Minecraft PE IOS ✅</em></strong>',
         url: 'https://ipas.cc/3jb6h7',
         img: 'https://i.postimg.cc/Y05W9Pv8/IMG-4773.png',
@@ -21,11 +26,6 @@ const apps = [
         url: 'https://ipas.cc/kbaova',
         img: 'https://i.postimg.cc/wMkzwh26/IMG-4719.jpg',
         descriptions: 'Link Cài Trực Tiếp V2.727 Cập Nhật Ngày 17/07/2026'
-    },{
-        name: '<strong><em> MENU Liên Quân IOS ✅</em></strong>',
-        url: 'https://ipas.cc/j4dwum',
-        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 17/07/2026'
     },{
         name: '<strong><em> MENU FF IOS ✅</em></strong>',
         url: 'https://ipas.cc/jgs0xk',

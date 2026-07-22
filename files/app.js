f81601e387742603c3f0eafeb95981f26a31f631
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-07-22 10:34:30 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -2,6 +2,11 @@
 
 const apps = [
 {
+        name: '<strong><em> MENU 8 Ball Pool IOS ✅</em></strong>',
+        url: 'https://ipas.cc/ymcfkg',
+        img: 'https://i.postimg.cc/Hxv3Pj2b/1783586272519-541494777965378080-584700966525922238-0b82b24f9f0cc544305843cc7a24dc0d.jpg',
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 22/07/2026'
+    },{
         name: '<strong><em> Minecraft PE IOS ✅</em></strong>',
         url: 'https://ipas.cc/ghs9dp',
         img: 'https://i.postimg.cc/Y05W9Pv8/IMG-4773.png',
@@ -30,11 +35,6 @@ const apps = [
         name: '<strong><em> MENU FF IOS ✅</em></strong>',
         url: 'https://ipas.cc/jgs0xk',
         img: 'https://i.postimg.cc/NfBCGPw3/1JSOL8E2V-AD5H4H.jpg',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 13/07/2026'
-    },{
-        name: '<strong><em> MENU 8 Ball Pool IOS ✅</em></strong>',
-        url: 'https://ipas.cc/ho0ftb',
-        img: 'https://i.postimg.cc/Hxv3Pj2b/1783586272519-541494777965378080-584700966525922238-0b82b24f9f0cc544305843cc7a24dc0d.jpg',
         descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 13/07/2026'
     },{
         name: '<strong><em> Proxy FF IOS ✅</em></strong>',

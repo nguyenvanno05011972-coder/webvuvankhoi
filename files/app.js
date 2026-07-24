1561d6007e1aadadf09e54738f10d7c7d3dc8208
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-07-24 21:59:23 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -1,7 +1,12 @@
 // APP DATA CONFIGURATION
 
 const apps = [
-{ 
+{
+        name: '<strong><em> MENU Liên Quân IOS ✅</em></strong>',
+        url: 'https://ipas.cc/1r3922',
+        img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 24/07/2026'
+    },{ 
         name: '<strong><em>Delta X Quốc Tế IOS  ✅</em></strong>',
         url: 'https://api.sub2s.com/key/deltaqtios',
         img: 'https://i.postimg.cc/wMkzwh26/IMG-4719.jpg',

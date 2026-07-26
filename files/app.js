34d46932d5fb463e3901cdad6ab854b26764665d
nguyenvanno05011972-coder <nguyenvanno05011972@gmail.com>
2026-07-26 18:46:40 +0700

    Update app.js

--- files/app.js
+++ files/app.js
@@ -5,17 +5,17 @@ const apps = [
         name: '<strong><em>Delta X VNG IOS Bản Gốc ✅</em></strong>',
         url: 'https://www.ngocbonggaming.com/2026/07/link-tai-delta-x-vng-ios-moi-nhat.html',
         img: 'https://i.postimg.cc/j5Pt7VJt/IMG-3155.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.730 Cập Nhật Ngày 25/07/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.730 Cập Nhật Ngày 26/07/2026'
     },{ 
         name: '<strong><em>Delta X VNG IOS Fix Lag ✅</em></strong>',
         url: 'https://sub2s.com/1785057275',
         img: 'https://i.postimg.cc/pXQNsVQs/IMG-3417.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.727 Cập Nhật Ngày 17/07/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.727 Cập Nhật Ngày 26/07/2026'
     },{ 
         name: '<strong><em>Delta X Quốc Tế IOS  ✅</em></strong>',
-        url: 'https://ipas.cc/4nkp7u',
+        url: 'https://sub2s.com/1785066336',
         img: 'https://i.postimg.cc/wMkzwh26/IMG-4719.jpg',
-        descriptions: 'Link Cài Trực Tiếp V2.730 Cập Nhật Ngày 25/07/2026'
+        descriptions: 'Link Cài Trực Tiếp V2.730 Cập Nhật Ngày 26/07/2026'
     },{
         name: '<strong><em> MENU FF IOS ✅</em></strong>',
         url: 'https://sub2s.com/1785053328',
@@ -23,17 +23,17 @@ const apps = [
         descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 26/07/2026'
     },{
         name: '<strong><em> Minecraft PE IOS ✅</em></strong>',
-        url: 'https://ipas.cc/5f01ar',
+        url: 'https://sub2s.com/1785066359',
         img: 'https://i.postimg.cc/Y05W9Pv8/IMG-4773.png',
-        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 25/07/2026'
+        descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 26/07/2026'
     },{
         name: '<strong><em> MENU Liên Quân IOS ✅</em></strong>',
-        url: 'https://ipas.cc/1r3922',
+        url: 'https://sub2s.com/1785066276',
         img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/7f/f9/70/7ff970e7-3b59-b769-34b5-e18c10e48990/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
-        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 25/07/2026'
+        descriptions: 'Link Cài Trực Tiếp Hack Map LQ Cập Nhật Ngày 26/07/2026'
     },{
         name: '<strong><em> MENU 8 Ball Pool IOS ✅</em></strong>',
-        url: 'https://ipas.cc/1w15ve',
+        url: 'https://sub2s.com/1785066381',
         img: 'https://i.postimg.cc/Hxv3Pj2b/1783586272519-541494777965378080-584700966525922238-0b82b24f9f0cc544305843cc7a24dc0d.jpg',
         descriptions: 'Link Cài Trực Tiếp Cập Nhật Ngày 25/07/2026'
     },{
